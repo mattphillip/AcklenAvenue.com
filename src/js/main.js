ec93292dbd8a9847fe9b1f9f83b9732f9dd5419c
Camilo Aguilar <camilo@acklenavenue.com>
2013-03-20 16:25:56 -0600

    Removed old version an update a version using initializr.com

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,1 @@
-$(document).ready(function(){
-	$('section[data-type="background"]').each(function(){
-		var $bgobj = $(this);
-		$(windows).scroll(function(){
-			var yPos = -($windows.scrollTop() / $bgobj.data('speed'));
-			var coords = '50%'+yPos+'px';
-			$bgobj.css({backgroundPosition: coords});
-		});
-	});
-});
+
