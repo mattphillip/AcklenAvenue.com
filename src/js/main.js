76f770d902834442f86eee9588bb1b33e71df232
Mario Maradiaga <mario@acklenavenue.com>
2013-03-26 00:28:36 -0600

    Added basic example animation for pie chart

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
-
+var canvas = document.getElementById("canvas");
+    canvas.height = 250;
+if (canvas.getContext){
+  	var ctx = canvas.getContext('2d');
+  	ctx.shadowColor = '#999';
+    ctx.shadowBlur = 40;
+    ctx.shadowOffsetX = 5;
+    ctx.shadowOffsetY = 5;
+    var img = document.getElementById("tree");
+    ctx.drawImage(img, 10, 10, 170, 150);
+} else {
+  // canvas-unsupported code here
+}
