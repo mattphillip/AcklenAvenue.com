1161aeee881f6a2ae327f4b73d202a0e162da2e4
Mario Maradiaga <mario@acklenavenue.com>
2013-03-26 14:57:54 -0600

    Implemented high charts alternative

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,53 @@
-var canvas = document.getElementById("canvas");
-    canvas.height = 250;
-if (canvas.getContext){
-  	var ctx = canvas.getContext('2d');
-  	ctx.shadowColor = '#999';
-    ctx.shadowBlur = 40;
-    ctx.shadowOffsetX = 5;
-    ctx.shadowOffsetY = 5;
-    var img = document.getElementById("tree");
-    ctx.drawImage(img, 10, 10, 170, 150);
-} else {
-  // canvas-unsupported code here
-}
+$(function () {
+        $('#chart').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                width: null,
+                height: 200
+            },
+            title:{
+                text: ""    
+            },
+            tooltip: {
+        	    enabled: false
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>';
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Browser share',
+                data: [
+                    ['SAS',   33.0],
+                    ['Internet Security',       34.0],
+                    ['.NET Development',    33.0],
+                ]
+            }]
+        });
+    });
+$(document).ready(function(){
+    var pieChartElements = $('path').splice(3,6);
+$(pieChartElements[0]).click(function(){
+    $("a[href='#SAS']").click();
+});
+$(pieChartElements[1]).click(function(){
+    $("a[href='#internetSecurity']").click();
+});
+$(pieChartElements[2]).click(function(){
+    $("a[href='#dotNetDevelopment']").click();
+});
+
+});
