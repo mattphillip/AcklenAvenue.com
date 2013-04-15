7a996818a75a58ef9767801a5d554de196b35a94
Camilo Aguilar <camilo@acklenavenue.com>
2013-04-15 09:02:05 -0600

    Attempt to fix flash issue on scrolling

--- js/main.js
+++ js/main.js
@@ -436,8 +436,9 @@ function scrollTo(section)
 {
 	if (section){
 		var sectionOffset = $(section).offset();
-		$('html, body').animate({scrollTop: sectionOffset.top }, "slow");
+		$('html, body').animate({scrollTop: sectionOffset.top }, 2000);
 	}
+	
 };
 
 $(document).ready(function() {
@@ -454,9 +455,61 @@ $(document).ready(function() {
   startCircularAvatars();
   drawTriangle();
   
-  	$("#menu a").click(function() {
-		scrollTo($(this).attr('href'));
-	});
+  	//$("#menu a").click(function() {
+		//scrollTo($(this).attr('href'));
+	//	return false;
+	//});
+  
+  /**/
+  
+  function filterPath(string) {
+  return string
+    .replace(/^\//,'')
+    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+    .replace(/\/$/,'');
+  }
+  var locationPath = filterPath(location.pathname);
+  var scrollElem = scrollableElement('html', 'body');
+ 
+  $('a[href*=#]').each(function() {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    if (  locationPath == thisPath
+    && (location.hostname == this.hostname || !this.hostname)
+    && this.hash.replace(/#/,'') ) {
+      var $target = $(this.hash), target = this.hash;
+      if (target) {
+        var targetOffset = $target.offset().top;
+        $(this).click(function(event) {
+          event.preventDefault();
+          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
+            location.hash = target;
+          });
+        });
+      }
+    }
+  });
+ 
+  // use the first element that is "scrollable"
+  function scrollableElement(els) {
+    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
+      var el = arguments[i],
+          $scrollElement = $(el);
+      if ($scrollElement.scrollTop()> 0) {
+        return el;
+      } else {
+        $scrollElement.scrollTop(1);
+        var isScrollable = $scrollElement.scrollTop()> 0;
+        $scrollElement.scrollTop(0);
+        if (isScrollable) {
+          return el;
+        }
+      }
+    }
+    return [];
+  }
+  
+  /**/
+  
   
 });
 
